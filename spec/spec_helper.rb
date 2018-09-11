b8d5004eda78a39dbcca1129e61ba57ab71ec670
ramoncord <ramonestii@hotmail.com>
2018-09-10 21:54:45 -0300

    initial setup

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require_relative '../app.rb'
+require 'rspec'
+require 'rack/test'
+
+set :environment, :test
+
+module RSpecMixin
+  include Rack::Test::Methods
+
+  def app
+    App
+  end
+end
+
+RSpec.configure do |c|
+  c.include RSpecMixin
